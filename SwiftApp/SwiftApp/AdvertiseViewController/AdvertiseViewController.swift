77dd920e68aca347405792621f7fa18b11eb8dd0
zengao <zengao@ifunbow.com>
2022-03-12 15:11:34 +0800

    多window 切换 广告页和主视图控制器

--- SwiftApp/SwiftApp/AdvertiseViewController/AdvertiseViewController.swift
+++ SwiftApp/SwiftApp/AdvertiseViewController/AdvertiseViewController.swift
@@ -19,7 +19,8 @@ class AdvertiseViewController: ZABaseViewController {
         super.viewDidLoad()
         view.backgroundColor = .brown
         view.addSubview(timerLabel)
-        timerLabel.text = String(seconds) + "s"
+//        timerLabel.text = String(seconds) + "s" // int 型转为 string
+        timerLabel.text = "\(seconds) s";// int 型转为 string
         timerLabel.textColor = .white
         timerLabel.textAlignment = .center
         timeCountDown()
@@ -34,9 +35,10 @@ class AdvertiseViewController: ZABaseViewController {
                   // 小于等于 0 时，结束 timer，并进行两个 rootViewController 的切换
                   if self!.seconds <= 0 {
                       self!.terminer()
+                  }else{
+                      self!.seconds -= 1
+                      self?.timerLabel.text = "\(self!.seconds) s";
                   }
-                  self!.seconds -= 1
-                  self?.timerLabel.text = String(self!.seconds) + "s"
               }
           })
           timer.resume()
@@ -44,12 +46,11 @@ class AdvertiseViewController: ZABaseViewController {
     
     func terminer() {
         timer.cancel()
-        switchRootController()
+//        switchRootController()
+        switchWindow()
     }
     
-    //
     // 一个 window 的情况：只用切换 rootViewController 就行
-    //
     func switchRootController() {
         let window = UIApplication.shared.windows.first!
         
@@ -63,9 +64,48 @@ class AdvertiseViewController: ZABaseViewController {
             UIView.setAnimationsEnabled(false)
             window.rootViewController = ZATabBarController()
             UIView.setAnimationsEnabled(old)
-            
         }, completion: { _ in
             // Do Nothing
+        })
+    }
+    
+    // 同样两种方式可以实现：广告页 -> 主页面：
+       // 1. 两个 window 来分别控制不同的业务，然后基于过渡动画来切换 window；
+       // 2. 一个 window，两个 vc，分别是 主vc 和 广告vc，通过修改 window.rootViewController 来完成；
+    func switchWindow() {
+        // 第2个 window（广告窗口）
+        let window = UIApplication.shared.windows.last!
+        
+//        UIView.transition(with: window, duration: 2.0, options: .transitionCrossDissolve) {
+//            window.alpha = 0
+//        } completion: { _ in
+//            // 切换到主 window，即我们的 ZATabBarController
+//            UIApplication.shared.windows.first?.makeKeyAndVisible()
+//        }
+
+        // 过渡动画：淡出
+        UIView.transition(with: window,
+                          duration: 0.5,
+                          options: .transitionCrossDissolve,
+                          animations: {
+            
+            // 临时保存 UIView 是否开启动画的状态（默认是开启）
+            // 之所以先禁止，是防止存在其它动画影响了当前动画
+            // ----------------------------------------
+            // 设置了禁止动画后：
+            // 1. 当前所有正在执行动画的没有任何影响
+            // 2. 未执行的将不会执行动画
+            // ----------------------------------------
+            // 因为我们这个回调是动画已经开始，所以，并不会被强制停止,
+            // 最后再恢复 UIView 是否开启动画的状态即可
+            let old = UIView.areAnimationsEnabled
+            UIView.setAnimationsEnabled(false)
+            window.alpha = 0
+            UIView.setAnimationsEnabled(old)
+            
+        }, completion: { _ in
+            // 切换到主 window，即我们的 ZATabBarController
+            UIApplication.shared.windows.first?.makeKeyAndVisible()
         })
     }
     
--- SwiftApp/SwiftApp/AppDelegate.swift
+++ SwiftApp/SwiftApp/AppDelegate.swift
@@ -11,15 +11,40 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // 多个 window：
+    // 第 1 个用于主app；
+    // 第 2 个用于显示广告页；
+    var windows: [UIWindow]?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 //        Thread.sleep(forTimeInterval: 3) //延长启动页时间
+        
+        /* 单window （切换rootViewController）
         window = UIWindow.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height))
         window?.backgroundColor = .white
         window?.rootViewController = AdvertiseViewController()
         window?.makeKeyAndVisible()
+         */
+        
+        
+        // 后一个 window 盖在前一个之上，可以通过：
+        // windows?[下标].makeKeyAndVisible() 来切换显示
+        // 多window 主视图控制器 和 广告视图 不共用同一个window
+        windows = [
+            addWindowWithVC(ZATabBarController()),
+            addWindowWithVC(AdvertiseViewController())
+        ]
         return true
+    }
+    
+    func addWindowWithVC(_ vc: UIViewController) -> UIWindow {
+        let window = UIWindow.init(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        return window
     }
 
     // MARK: UISceneSession Lifecycle
